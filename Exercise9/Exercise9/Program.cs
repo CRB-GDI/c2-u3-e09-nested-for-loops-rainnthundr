148bdf530c5cc33988647cf9c7c58438eaa1d54d
rainnthundr <145371191+rainnthundr@users.noreply.github.com>
2024-02-29 12:05:56 -0500

    finished problem 1

---
+++ Exercise9/Exercise9/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem 1
+
+            Console.Write("enter number of rows: ");
+            int row = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < row; i++)
+            {
+                Console.WriteLine(row);
+            }
+          
+            Console.WriteLine("enter the row width: ");
+            int numberAsterisks = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberAsterisks; i++)
+            {
+                Console.Write("*");
+            }
+
+            //Problem 2
+
+            Console.Write("enter the number of row width: ");
+            int width = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i >= width; i++)
+            {
+                Console.Write("*");
+            }
+        }
+            
+
+
+        
+    }
+}
