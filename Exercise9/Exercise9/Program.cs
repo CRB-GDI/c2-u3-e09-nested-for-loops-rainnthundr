c89498f1f856acd4c64832dbc0132b7bdbd35e0d
rainnthundr <145371191+rainnthundr@users.noreply.github.com>
2024-02-29 12:57:26 -0500

    Update Program.cs

--- Exercise9/Exercise9/Program.cs
+++ Exercise9/Exercise9/Program.cs
@@ -5,24 +5,39 @@
         static void Main(string[] args)
         {
             //Problem 1
-
+            //prompting for rows and it's width
             Console.Write("enter number of rows: ");
             int x = int.Parse(Console.ReadLine());
-            
+
             Console.WriteLine("enter the row width: ");
             int y = int.Parse(Console.ReadLine());
 
-            for (int rowsPrinted = 0; rowsPrinted < x; rowsPrinted++)
+            //setting up the loop and printing the block
+            for (int rows = 0; rows < x; rows++)
             {
                 for (int width = 0; width < y; width++)
                 {
                     Console.Write("*");
                 }
                 Console.WriteLine();
-            }    
+            }
 
-             
+            {
+                //Problem 2
+                //prompting the number of rows
+                Console.Write("whats the row width?: ");
+                int z = int.Parse(Console.ReadLine());
+
+                for (int rowWidth = 0; rowWidth < z; rowWidth--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
         }
+
+           
             
 
 
